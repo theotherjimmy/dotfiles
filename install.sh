8e00c59fa7514df44aee1ae7318f20f658284fbe
Jimmy Brisson <theotherjimmy@gmail.com>
2019-06-25 09:42:42 -0500

    Add SpaceVim configuration

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/usr/bin/bash
 BASEDIR=$(dirname $0)
 cd ${BASEDIR}
 
 ln -sf ${PWD}/xinitrc ${HOME}/.xinitrc
 
 mkdir -p ${HOME}/.config
+
 if [ -d ${HOME}/.config/alacritty ] ; then
   mv ${HOME}/.config/alacritty ${HOME}/.config/alacritty.bak ;
 fi
 ln -sf ${PWD}/alacritty ${HOME}/.config/alacritty
+
+if [ -d ${HOME}/.SpaceVim.d ] ; then
+    mv ${HOME}/.SpaceVim.d ${HOME}/.SpaceVim.d.bak ;
+fi
+ln -sf ${PWD}/SpaceVim.d ${HOME}/.SpaceVim.d
