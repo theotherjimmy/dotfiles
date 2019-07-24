573ca46d98243a32338bc47aeaba586fda097d59
Jimmy Brisson <theotherjimmy@gmail.com>
2019-07-24 09:20:12 -0500

    Remove unused parameter to psi fish function

--- fish/functions/psi.fish
+++ fish/functions/psi.fish
@@ -1,6 +1,5 @@
-function psi -a search-term -w pacman
-    set -l to_install (pacman -Ss $search-term | sed -e "/    /d" | sk | sed -e 's/ .*$//')
-    echo $to_install
+function psi -w pacman
+    set -l to_install (pacman -Ss | sed -e "/    /d" | sk | sed -e 's/ .*$//')
     if test -n "$to_install" ;
         sudo pacman -S $to_install
     end
