62127379f694a41813a28ae29a5b33c352ef5d14
Jimmy Brisson <theotherjimmy@gmail.com>
2019-07-24 09:20:48 -0500

    Use terminal colors for fish syntax highlighting

---
+++ fish/conf.d/syntax-color.fish
@@ -0,0 +1,14 @@
+set fish_color_normal normal
+set fish_color_command brblue
+set fish_color_args blue
+set fish_color_quote bryellow
+set fish_color_redirection brgreen
+set fish_color_end white
+set fish_color_error red --bold
+set fish_color_comment green
+set fish_color_match magenta
+set fish_color_selection cyan
+set fish_color_search_match brwhite
+set fish_color_operator brcyan
+set fish_color_escape yellow
+set fish_color_autosuggestion brblack
