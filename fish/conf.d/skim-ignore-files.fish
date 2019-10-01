a65efec622d11cb42ffa61f34a20ef233956685e
Jimmy Brisson <theotherjimmy@gmail.com>
2019-10-01 16:53:51 -0500

    Fix skim ignore files

--- fish/conf.d/skim-ignore-files.fish
+++ fish/conf.d/skim-ignore-files.fish
@@ -1,1 +1,1 @@
-set SKIM_DEFAULT_COMMAND "fd --type f || git ls-tree -r --name-only HEAD || rg --files || find ."
+export SKIM_DEFAULT_COMMAND="fd --type f || git ls-tree -r --name-only HEAD || rg --files || find ."
