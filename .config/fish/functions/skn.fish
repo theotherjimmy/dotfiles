7f67700c653590365567813ae1516b91c0152819
Jimmy Brisson <theotherjimmy@gmail.com>
2019-12-16 09:38:36 -0600

    Don't hard code editor in `skn` function
    This will allow me to swap out editors at whim

--- .config/fish/functions/skn.fish
+++ .config/fish/functions/skn.fish
@@ -1,7 +1,7 @@
 function skn -d "skim files to open with nvim"
   set -l files (sk -m | tr "\n" " ")
   if test -n "$files" 
-    commandline "nvim "$files
+    commandline "$EDITOR "$files
     commandline -f execute
   end
 end
