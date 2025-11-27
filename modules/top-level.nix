76e3844f38f8122debf6d620772b964b899c4514
Jimmy Brisson <jbrisson@linux.ibm.com>
2025-11-27 09:29:10 -0600

    Move on from garish colors

--- modules/top-level.nix
+++ modules/top-level.nix
@@ -22,10 +22,11 @@
       #"${pkgs.base16-schemes}/share/themes/nebula.yaml" # faintly purple
       #"${pkgs.base16-schemes}/share/themes/nova.yaml" # light blue & orange
       #"${pkgs.base16-schemes}/share/themes/rose-pine.yaml" # very dark purple
-      "${pkgs.base16-schemes}/share/themes/tarot.yaml" # dark purple to salmon      #"${pkgs.base16-schemes}/share/themes/caroline.yaml" # very red, with more red
+      #"${pkgs.base16-schemes}/share/themes/tarot.yaml" # dark purple to salmon      #"${pkgs.base16-schemes}/share/themes/caroline.yaml" # very red, with more red
       #"${pkgs.base16-schemes}/share/themes/everforest.yaml" # plesently blue to green
       #"${pkgs.base16-schemes}/share/themes/terracotta-dark.yaml" # brown w/ green highlights
       #"${pkgs.base16-schemes}/share/themes/valua.yaml" # green w/ yellow, green & purple
+      "${pkgs.base16-schemes}/share/themes/vulcan.yaml" # dark blue & orange highlights
     ;
   };
   home.keyboard = {
